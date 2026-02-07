7b872dac93e2306722ea2f84fceb332a8410a152
dev0jha <ojha6773@gmail.com>
2026-02-07 16:36:47 +0530

    New File

---
+++ Microsoft.VSCode_profile.ps1
@@ -0,0 +1,35 @@
+function bro {
+    bun run dev
+}
+
+function py {
+    python @args
+}
+
+function ga {
+    git add @args
+}
+
+function gcommit {
+    git commit -m ($args -join " ")
+}
+
+function gs {
+    git status
+}
+
+function gpush{
+  git push origin main
+}
+
+function bi {
+    bun install @args
+}
+function gfetch{
+    git fetch origin @args
+}
+function ginit{
+    git init
+}
+
+
